e5072458ceae0ed4694d89184cb13c8563ffc509
An <nguyenhuuhoangan2504@gmail.com>
2025-05-08 17:34:05 +0700

    change App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOW -development!!!
+          Learn React NOW - test!!!
         </a>
       </header>
     </div>
