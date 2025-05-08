8b71cb8147d43226b9a2cd9b793be21fedbab12e
An <nguyenhuuhoangan2504@gmail.com>
2025-05-08 18:08:09 +0700

    App.js updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - production!!!
+          Learn React - production!!
         </a>
       </header>
     </div>
