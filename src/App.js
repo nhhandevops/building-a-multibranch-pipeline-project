2fd66308f9589dc5f3dc325349b7495516586fd0
An <nguyenhuuhoangan2504@gmail.com>
2025-05-09 11:00:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOW - sandbox!!!
+          Learn React NOW - sandbox!!
         </a>
       </header>
     </div>
